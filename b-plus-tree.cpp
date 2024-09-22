2ebba68b043f442fa13377d65188de236b3d63cc
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2024-09-22 16:06:09 +0800

    feat: implement insert func

--- b-plus-tree.cpp
+++ b-plus-tree.cpp
@@ -1,9 +1,10 @@
 #include <stdlib.h> 
+#include <iostream>
 #include <vector>
 
 using std::vector; 
-
-
+using std::cout;
+using std::endl;
 
 int MAX = 256; 
 
@@ -26,16 +27,16 @@ class Node {
     vector<Node*> ptr;
 
     bool isLeaf;
-    Node *nextLeaf; 
+    //Node *nextLeaf; 
     friend class BPTree;
 
 public:
     Node() {
         key.resize(MAX);  // Resizes the vector to hold 'MAX' elements
         ptr.resize(MAX + 1);  // Resizing to hold MAX + 1 child pointers
-        size = MAX;
+        size = 0;
         isLeaf = true;
-        nextLeaf = NULL; 
+        //nextLeaf = NULL; 
     }
 };
 
@@ -44,7 +45,7 @@ public:
 class BPTree{
     Node *root; 
 
-    //utility func to get the leaf node containing the value 
+    //utility func to get the possible leaf node containing the value 
     Node *getLeafNode(float value){ 
         if (!root) return NULL;
 
@@ -82,7 +83,9 @@ class BPTree{
         return NULL; 
     }
 
-    
+    void insertInternalNode(Node *parent, Node *child, Data *data){ 
+        
+    }
     public: 
         BPTree(){
             root = NULL; 
@@ -123,10 +126,103 @@ class BPTree{
                     if (data->value > upperBound)
                         return res; 
                 }
-                leafNode = leafNode->nextLeaf; 
+                leafNode = leafNode->ptr[leafNode->size]; 
             }
           
             return res; 
+        }
+
+
+        void insert(Data *data){ 
+            //empty tree
+            if (!root){
+                root = new Node();
+                root->size++;
+                root->key.push_back(data); 
+                return; 
+            }
+
+
+            //get the correct leafnode 
+            Node *leafNode = getLeafNode(data->value);
+            if (!leafNode){
+                cout << "Error in inserting new data: " << data->value << endl; 
+                return; 
+            }
+          
+
+            for (auto &d: leafNode->key){
+                //check if key with same value already exists
+                if (d->value == data->value){
+                    d->addresses.push_back(data->addresses[0]);
+                    return; 
+                }
+            }
+            
+            //insert new key into cur leaf node 
+            //leafNode is not full yet
+            if (leafNode->size < MAX){
+                int i = 0; 
+                while (data->value > leafNode->key[i]->value and i < leafNode->size) 
+                    i++;
+
+                //make space for insertion
+                for (int j = leafNode->size; j > i; j--)
+                    leafNode->key[j] = leafNode->key[j - 1];
+
+                leafNode->key[i] = data;
+                leafNode->size++;
+                leafNode->ptr[leafNode->size] = leafNode->ptr[leafNode->size - 1];
+                leafNode->ptr[leafNode->size - 1] == NULL; 
+            } else { 
+                //leafNode is full make a new node
+                Node *newLeaf = new Node();
+
+                vector<Data *> temp(MAX+1);
+                int i = 0; 
+                for (auto key: leafNode->key)
+                    temp[i++] = key;
+                
+                i = 0; 
+                while(data->value > temp[i]->value and i < MAX)
+                    i++;
+                for (int j = MAX; j > i; j--)
+                    temp[j] = temp[j - 1]; 
+                temp[i] = data; 
+
+
+                //set up new leaf;
+                leafNode->size = (MAX + 1) / 2;
+                newLeaf->size = MAX + 1 - (MAX + 1) / 2;
+
+                leafNode->ptr[leafNode->size] = newLeaf;
+                newLeaf->ptr[newLeaf->size] = leafNode->ptr[MAX];
+
+                leafNode->key.clear();
+                for (int i = 0; i < leafNode->size; i++)
+                    leafNode->key[i] = temp[i];
+
+                for (int j = 0, i = leafNode->size; j < newLeaf->size; i++, j++)
+                    newLeaf->key[j] = temp[i]; 
+
+
+                //modify the parent node
+                //cur leaf is root node
+                if (leafNode == root){
+                    Node *newRoot = new Node();
+                    newRoot->key[0] = newLeaf->key[0];
+                    newRoot->ptr[0] = leafNode;
+                    newRoot->ptr[1] = newLeaf;
+                    newRoot->size++; 
+                    root = newRoot; 
+                } else{ // cur leaf is not root
+
+                    // get parent node
+                    Node *parent = findParent(root, leafNode);
+
+                    insertInternalNode(parent, newLeaf, newLeaf->key[0]); 
+                }
+            }
         }
 
 }; 
