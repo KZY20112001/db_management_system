fa6be461484e62f21d5b406da6b4cc1608a62928
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2024-10-03 01:08:22 +0800

    test: duplicate values

--- b-plus-tree-test.cpp
+++ b-plus-tree-test.cpp
@@ -12,11 +12,11 @@ int main(){
 
     //vector<int> values = {3, 4, 6, 9, 10, 11,12,13,20,22};
     //vector<int> values = {1, 4, 7, 10, 17, 21,31,25,19,20};
-    vector<int> values = {10,20,40,50,70,80,90,100, 30,60}; 
+    vector<int> values = {10,20,30, 40,50,60, 70,80,90,100, 30,60, 10,10,10,10,10,20,20,20}; 
     vector<KeyStruct> keys;
 
-    //for testing all keys will have 1,1 default location values as only keys are important for building b+ tree
-    Record_Location testLocation = {10, 19};
+    //for testing all keys will have 10,20 default location values as only keys are important for building b+ tree
+    Record_Location testLocation = {10, 20};
     for (int v: values){
         
         KeyStruct key = {float(v), {testLocation}};
@@ -29,9 +29,23 @@ int main(){
         cout << "Inserting: " << key.value << endl; 
         tree.insert(key); 
     }
-    cout << "Searching " << endl; 
-    KeyStruct res = tree.search(10);
-    cout << "result: " << res.value << endl; 
+
     tree.displayAllKeys();
+
+    //search each key to test
+    for (auto v: values){ 
+        KeyStruct res = tree.search(v);
+        if (res.value == -1)
+            continue; 
+        cout << "Number of results for key "<< v << " : " << res.addresses.size() << endl; 
+        for (auto address: res.addresses){
+            cout << "Block Num: " << address.blocknum << ", Offset: " << address.offset << endl; 
+        }
+        cout << endl; 
+    }
+
+    //search error key
+    KeyStruct res = tree.search(0);
+    cout << "Not Found for 0: " << res.value << endl; 
     return 0; 
 }   
